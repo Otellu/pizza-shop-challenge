137f29b92c54f326499b19e4ac3a446b593c24ec
Bhavik Mojidra <bhavik.m@brilworks.com>
2025-06-30 17:32:18 +0530

    webhook todo

--- backend/src/controllers/webhookController.js
+++ backend/src/controllers/webhookController.js
@@ -1,8 +1,5 @@
 const deliveryUpdate = async (req, res) => {
-  // Simulate 5s delay
-  setTimeout(() => {
-    res.json({ status: 'delivered', orderId: req.body.orderId });
-  }, 5000);
+  // TODO: handle incoming webhook event and update the order
 };
 
 module.exports = {
